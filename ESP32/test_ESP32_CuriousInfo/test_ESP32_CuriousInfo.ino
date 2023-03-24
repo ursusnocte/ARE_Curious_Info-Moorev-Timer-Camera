ba77d67447d69517ea768c9b18f925b16f63c777
Ursus Nocte <Foxdelta8@users.noreply.github.com>
2023-03-24 16:57:32 +0100

    new file:   ESP32/test_ESP32_CuriousInfo/test_ESP32_CuriousInfo.ino

---
+++ ESP32/test_ESP32_CuriousInfo/test_ESP32_CuriousInfo.ino
@@ -0,0 +1,28 @@
+/*
+Script de test ARE Moorev-Timer-Caméra hibernation ESP32 : Allume la led intégré pendant 3 secondes toutes les 60 secondes
+*/
+
+#define uS_TO_S_FACTOR 1000000
+#define TIME_TO_SLEEP  60
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  Serial.println("----------------------");
+  Serial.println("Allumage Led pour 3 secondes");
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(3000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+
+   //Configuration du timer
+   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+   Serial.println("ESP32 réveillé dans " + String(TIME_TO_SLEEP) + " secondes");
+
+   delay(100); //Pour être sur que le texte à fini de s'afficher
+   
+   esp_deep_sleep_start();
+}
+
+void loop(){}
